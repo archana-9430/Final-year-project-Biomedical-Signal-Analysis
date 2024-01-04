46550551dd06cfcb5a11ce88dc8e5fa1cfc00b3f
Shwashwat <shwashwatdas@gmail.com>
2024-01-04 13:49:37 +0530

    Update 4.feature_ext.py

--- code/BIDMC/4.feature_ext.py
+++ code/BIDMC/4.feature_ext.py
@@ -2,20 +2,32 @@ annotated_file = "Annotated_125Hz\\bidmc03m.csv"
 
 import pandas as pd
 import math
+import numpy as np
 from scipy.stats import entropy, skew, kurtosis
 
-def shanon_entropy(segment) -> int: 
-    entr = 0
-    for i in range(0, len(segment)):
-        entr += segment[i]*math.log(segment[i], 2)
-    return entr
+# def shanon_entropy(segment) -> int: 
+#     entr = 0
+#     for i in range(0, len(segment)):
+#         entr += segment[i]*math.log(segment[i], 2)
+#     return entr
+
+def shannon_entropy(time_series):
+    # Calculate the probability distribution
+    p = np.histogram(time_series)[0]
+    p_normalized = p / float(np.sum(p))
+    p_normalized = p_normalized[np.nonzero(p_normalized)]
+    
+    # Compute the Shannon entropy
+    H = entropy(p_normalized, base=2)
+    
+    return H
 
 def extract_features(segment):
     features = {}
-    value_counts = segment.value_counts(normalize=True)
+    # value_counts = segment.value_counts(normalize=True)
  
-    features['entropy'] = entropy(value_counts, base=2)
-    
+    # features['entropy'] = entropy(value_counts, base=2)
+    features['entropy'] = shannon_entropy(segment)
     features['skewness'] = skew(segment)
     features['kurtosis'] = kurtosis(segment)
     
