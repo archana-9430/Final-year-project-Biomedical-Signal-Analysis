0ca75e6d5ec3baa32664630850219ab22916a0f1
archana-9430 <archanakumari943064@gmail.com>
2024-01-09 09:05:46 +0530

    updated SVM model

--- code/BIDMC/5.SVM_model.py
+++ code/BIDMC/5.SVM_model.py
@@ -8,10 +8,10 @@ gama = 'auto'
 c = 2
 
 # k of k flod cross validation
-k = 5 # change if you want to experiment
+k = 9 # change if you want to experiment
 
 # class list
-class_list = ["1" , "2"] # good segn = 1 , corrupted signal = 2
+class_list = ["1" , "2"] # good signal = 1 , corrupted signal = 2
 
 # ~~~~~~~LIBRARIES~~~~~~~
 import pandas as pd
@@ -34,25 +34,24 @@ from sklearn.metrics import classification_report
 # ~~~~~~~~~~~~END LIBRARIES~~~~~~~~~~~~~~~~~~~~~
 
 def create_train_classifier(X_train_data, y_train_data):
-    # fit the decision tree model into the train set
-    # classifier = RandomForestClassifier(n_estimators = num_trees , random_state = rand_state , criterion = split_criteria)
+    # fit the SVM model into the train set
     classifier = svm.SVC(kernel = kern, gamma = gama, C = c)
     classifier.fit(X_train_data, y_train_data)
     return classifier
 
-def k_fold_s_crossval(x_train_data, y_train_data, k, classifier):
+def k_fold_s_crossval(X_train_data, y_train_data, k, classifier):
     # k fold cross validation
     rskf = RepeatedStratifiedKFold(n_splits = k, n_repeats = k, random_state = rand_state)
-    result = cross_val_score(classifier, x_train_data, y_train_data, cv = rskf)
+    result = cross_val_score(classifier, X_train_data, y_train_data, cv = rskf)
     print(result)
     print("Avg accuracy on train set: {}".format(result.mean()))
 
 
-def test_n_results(x_test_data , y_test_data , classifier):
+def test_n_results(X_test_data , y_test_data , classifier):
 
     # Now TESTING the model and showing the results:
     # confusion matrix 
-    test_pred_svm = classifier.predict(x_test_data)
+    test_pred_svm = classifier.predict(X_test_data)
     confusion_matrix = metrics.confusion_matrix(y_test_data , test_pred_svm)
     matrix_df = pd.DataFrame(confusion_matrix)
     ax = plt.axes()
@@ -68,7 +67,7 @@ def test_n_results(x_test_data , y_test_data , classifier):
     # classification report
     print("\nClassification Report:\n")
     print(classification_report(y_test_data, test_pred_svm))
-    print("\nAvg score on test dataset = {}".format(classifier.score(x_test_data , y_test_data)))
+    print("\nAvg score on test dataset = {}".format(classifier.score(X_test_data , y_test_data)))
     # print("\nNumber of Estimators = {}".format(num_trees))
     print("C = {}".format(c))
 
@@ -77,6 +76,7 @@ def svm_model(annotated_file , features_file):
     features = pd.read_csv(features_file)
     annotated_data = pd.read_csv(annotated_file)
     labels = annotated_data.iloc[0] # this will exract the annotation 2nd row
+    features = features.iloc[:, 1:]
 
     # split the dataset using test_train_split() function
     X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size = test_fraction, random_state = rand_state, stratify = labels)
@@ -84,6 +84,7 @@ def svm_model(annotated_file , features_file):
     clf = create_train_classifier(X_train , y_train)
     k_fold_s_crossval(X_train , y_train , k , clf)
     test_n_results(X_test , y_test , clf)
+    print(clf)
 
 
 svm_model(annotated_csv_path , features_file_path)
