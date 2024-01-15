c045cce83be9a0aa70791fec51f06624733dd14f
archana-9430 <archanakumari943064@gmail.com>
2024-01-16 02:44:07 +0530

    Generate csv PPG data from MIMIC 3, BIDMC, MIMIC PERForm datasets

--- code/Code/1.wfdb2csv.py
+++ code/Code/1.wfdb2csv.py
@@ -1,18 +1,24 @@
 ''''
 This file converts the MIMIC III Waveform dataset (.dat and .hea files) to csv files. 
 It extracts the column for PLETH, removes the NaN values and stores it to the csv file.
+It also extracts PPG data from BIDMC and MIMIC PERForm AF dataset and stores it to a csv file.
+All the csv files generated is passed to a common directory.
 '''
-
-path = '..\\Mixed_dataset\\MIMIC'
-file_dir = '..\\Mixed_dataset\\MIMIC\\'
 csv_path = 'Csv_data\\'
+#MIMIC
+mimic_path = '..\\Mixed_dataset\\MIMIC'
 
+#BIDMC
+bidmc_csv_path = "..\\Mixed_dataset\\BIDMC\\bidmc_data_all_125Hz_8Min.csv"
+
+#MIMIC PERForm AF
+perform_dir = "..\\Mixed_dataset\\mimic_perform_af_csv"
 
 import os
 import wfdb
 import pandas as pd
 
-def convert_to_csv(record_name, output_csv_path):
+def dat_to_csv(record_name, output_csv_path):
     # Read the record
     record = wfdb.rdrecord(record_name)
 
@@ -28,14 +34,56 @@ def convert_to_csv(record_name, output_csv_path):
 
     # Save to CSV
     df.to_csv(output_csv_path, index=False)
+     
+def mimic():
+    # Get the list of all .dat and .hea files
+    dir_list = os.listdir(mimic_path)
+    print(dir_list)
+    print(len(dir_list))
+
+    for i in range(len(dir_list)):
+        if i%2 == 0:
+            dat_to_csv(f"{mimic_path}\\{dir_list[i][:-4]}", csv_path + dir_list[i][:-3] + "csv")
+            
+def split_csv_columns(input_csv_path, csv_path):
+    # Read the CSV file
+    df = pd.read_csv(input_csv_path)
+
+    # Iterate over columns and create separate CSV files
+    for column_name in df.columns:
+        column_data = df[column_name]
+        output_csv_path = csv_path + f"{column_name}.csv"
+        column_data.to_csv(output_csv_path, index=False)
+        
+def bidmc():
+    split_csv_columns(bidmc_csv_path, csv_path)
     
+def extract_ppg_col(input_csv_path, output_csv_path):
+     # Read the CSV file
+    df = pd.read_csv(input_csv_path)
 
-# Get the list of all .dat and .hea files
-dir_list = os.listdir(path)
-print(dir_list)
-print(len(dir_list))
+    # Extract the "PPG" column
+    ppg_column = df["PPG"]
+    # Create a new DataFrame with only the "PPG" column
+    ppg_df = pd.DataFrame({"PPG": ppg_column})
 
-for i in range(len(dir_list)):
-    if i%2 == 0:
-        convert_to_csv(file_dir + dir_list[i][:-4], csv_path + dir_list[i][:-3] + "csv")
+    # Save the new DataFrame to a CSV file
+    ppg_df.to_csv(output_csv_path, index=False)
+    
+def mimic_perform_af():
+    dir_list = os.listdir(perform_dir)
+    print(dir_list)
+    print(len(dir_list))
+    # Filter only CSV files
+    csv_files = [file for file in dir_list if file.lower().endswith('.csv')]
+    for csv_input in csv_files:
+        output_csv_path = csv_path + f"{csv_input}.csv"
+        extract_ppg_col(f"{perform_dir}\\{csv_input}", output_csv_path)
 
+
+##Uncomment this code for running this code
+# mimic()
+# bidmc()
+# mimic_perform_af()
+
+    
