b72e571c7b2411d4d44edf629e44973794fc767b
Shwashwat <shwashwatdas@gmail.com>
2024-01-18 22:58:36 +0530

    Added DaLiA data and noise feathers

--- assests/Code/segmentation.py
+++ assests/Code/segmentation.py
@@ -7,12 +7,10 @@ sampling_freq = 125 # sampling freq in HERTZ
 window_len_sec = 10
 stride_len_sec = 6
 # in seconds
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# # global trimming variables
-# trim_ask = True
-# global_trim = False # set it to True if you want discard, matters if discard_ask is False
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 def plot_signal(x : list ,y : list , x_label = None , y_label = None , title = None):
@@ -23,37 +21,6 @@ def plot_signal(x : list ,y : list , x_label = None , y_label = None , title = N
     plt.title(title)
     plt.show()
     plt.close()
-
-# # For cutting out first and last 0.5 s
-# def trim_signal(original_signal:list , ask = False , time_stamp_original: list = NULL): 
-# 
-#     if(ask == True):
-#         should_discard = input("\n Need to discard first and last 0.5 sec?(Y/N , Default: N) ") # discards if this variable is set to True
-#     else:
-#         should_discard = 'N'
-# 
-#     if((should_discard == ('y' or 'Y')) or (global_trim == True) and time_stamp_original != NULL):
-#         discard_interval = 0.5
-#         sample_discard = int(discard_interval * sampling_freq)
-#         signal = original_signal[sample_discard : ]
-#         time_stamp = time_stamp_original[sample_discard : ]
-#         signal_len = len(signal)
-#         signal = signal[ : signal_len - sample_discard]
-#         time_stamp = time_stamp[ : signal_len - sample_discard]
-#   
-#     elif((should_discard == ('y' or 'Y')) or (global_discard == True) and time_stamp_original == NULL):
-#         discard_interval = 0.5
-#         sample_discard = int(discard_interval * sampling_freq)
-#         signal = original_signal[sample_discard : ]
-#         signal_len = len(signal)
-#         signal = signal[ : signal_len - sample_discard]
-# 
-#     else:
-#         signal = original_signal
-#         time_stamp = time_stamp_original
-# 
-#     return time_stamp , signal
-    
 
 def segmentator():
     # extract original signal from the csv file and plot it
@@ -66,36 +33,22 @@ def segmentator():
 
     # calculate some values
     signal_len = ppg_df.shape[0]
+    print(f"Signal Len = {signal_len}")
     stride_samples = stride_len_sec * sampling_freq
     samples_per_window = int(window_len_sec * sampling_freq)
     num_segments = ( signal_len -  samples_per_window + stride_samples ) / stride_samples
-    print("\nNumber of segments = {}".format(num_segments))
+    print(f"\nNumber of segments = {num_segments}")
 
     int_num_segments = int(num_segments)
-    print("\nInteger Number segment = {}".format(int_num_segments))
+    print(f"\nInteger Number segment = {int_num_segments}")
     segmented_df = pd.DataFrame() # Create an empty dataframe to contain annotated data
 
     for i in range(int_num_segments):
         # take out the segment from the total signal
         current_segment = ppg_data[i * stride_samples : i * stride_samples + samples_per_window]
         # plot_signal(range(len(current_segment)) , current_segment , "Samples", "PPG Signal" , "Segment {}".format(i))
-        segmented_df["Segment {}".format(i)] = current_segment
-    
-    # if(num_segments % 1):
-    #     # calculate the remaining sample number
-    #     remaining_sample_num = signal_len - (samples_per_window + i * stride_samples)
-
-    #     i += 1
-    #     # take out the segment from the total signal and plot it
-    #     current_segment = ppg_data[i * stride_samples : i * stride_samples + remaining_sample_num]
-    #     plot_signal(range(len(current_segment)) , current_segment , "Samples", "PPG Signal" , "Last Segment {}".format(i))
-
-    #     discard_seg = input("\nDiscard the last segment(y/n , Default: n)? ")
-    #     if(discard_seg == 'n'):
-    #         zero_padd = [ 0 for i in range(samples_per_window - remaining_sample_num) ]
-    #         current_segment += zero_padd
-    #         segmented_df["Segment {}".format(i)] = current_segment
+        segmented_df[f"Segment {i + 1}"] = current_segment
     
     segmented_df.to_csv(path_or_buf = segmented_csv_path , index = False , header = None) #save the file
-
+    
 segmentator()
