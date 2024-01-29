e78b78fda6d6fec65c457591218c2ba9b30c35dd
Shwashwat <shwashwatdas@gmail.com>
2024-01-29 21:50:50 +0530

    Added code for renaming files

--- assests/Code/2.1renaming.py
+++ assests/Code/2.1renaming.py
@@ -2,20 +2,27 @@
 segmented_folder = "10sec_segmented_data"
 
 import os
-import pandas as pd
 from pprint import pprint
 
 def rename():
   csv_list = os.listdir(segmented_folder)
-  name_dict = {}
-  for i in range(len(csv_list)):
-    original_path = f"{ten_sec_folder}\\{csv_list[i]}"
-    renamed_path = f"{ten_sec_folder}\\patient {i}.csv"
-    os.rename(original_path , renamed_path)
-    name_dict[original path] = renamed_path
+  name_string = ""
 
-  df = pd.DataFrame(name_dict)
-  df.to_csv(path_or_buf = f"{ten_sec_folder}\\mapping.csv" , index = False)
-  pprint(name_dict)
+  for i in range(len(csv_list)):
+    original_path = f"{segmented_folder}\\{csv_list[i]}"
+    renamed_path = f"{segmented_folder}\\patient_{i}.csv"
+
+    # rename the file
+    os.rename(original_path , renamed_path)
+
+    # add it the string
+    name_string += f"{original_path} -> {renamed_path}\n" 
+
+  pprint(name_string)
+
+  # save to text 
+  txt_file_name = f"{segmented_folder}\\mapping.txt"
+  with open( txt_file_name , "a") as txt_file:
+    txt_file.write(name_string)
   
-    
+rename()
