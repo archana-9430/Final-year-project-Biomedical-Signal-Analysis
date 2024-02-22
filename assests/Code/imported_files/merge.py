58b0d1a5a8e2adf782697511c23c9f34c3c04ef2
Archana Kumari <78868726+archana-9430@users.noreply.github.com>
2024-02-22 16:24:27 +0530

    fix merge error

--- assests/Code/imported_files/merge.py
+++ assests/Code/imported_files/merge.py
@@ -24,22 +24,20 @@ def merge_csv(input_folder, merge_path , save : bool = True):
     merged_df = pd.DataFrame()
 
     csv_files = [file for file in dir_list if file.endswith('.csv')]
-
     for file in csv_files:
         file_path = os.path.join(input_folder, file)
         filename_parts = os.path.splitext(file)[0].split('_')  # Split filename using "_"
-        file_number = filename_parts[1]  # Extract patient number
+        file_number = filename_parts[1]  # Extract prefix
         df = pd.read_csv(file_path, header=None)
         first_row = df.iloc[0].values
-        for value in first_row:
-            second_part = value.split(" ")[1]
-            value = file_number + "_" + second_part
+        for i in range(len(first_row)):
+            second_part = first_row[i].split(" ")[1]  
+            first_row[i] = file_number + "_" + second_part
         df[:0] = first_row
         # print(df[0])
         merged_df = pd.concat([merged_df, df], axis=1)
 
     if save:
         merged_df.to_csv(merge_path, index=False, header=False)
-    else:
-        return merged_df
+
     
