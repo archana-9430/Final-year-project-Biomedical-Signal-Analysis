3abba269e451ff3443899af3e15405069b330db0
Archana Kumari <78868726+archana-9430@users.noreply.github.com>
2024-02-24 22:10:36 +0530

    update clustering

--- assests/Code/clustering.py
+++ assests/Code/clustering.py
@@ -1,19 +1,22 @@
 '''cluster the segments of patients based on their annotations and
 then find the Euclidean distances from the cluster centers. '''
 
-from imported_files.paths_n_vars import intra_annotated_file
+from imported_files.paths_n_vars import intra_annotated_file, features_file
 
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.metrics import pairwise_distances
 
-df = pd.read_csv(intra_annotated_file)
+# df = pd.read_csv(intra_annotated_file)
+df = pd.read_csv(features_file)
 annotations = df.iloc[1].values
 
 # Drop patient IDs and annotations
-data = df.drop([0, 1])
-X = data.values.astype(float)
+data = df.drop([0])
+X = data.astype(float)
+print(type(X))
+print(X)
 
 #Elbow method
 wcss = []
@@ -29,12 +32,14 @@ plt.ylabel('WCSS')
 plt.show()
 
 #Optimal number of clusters from the elbow method plot
-n_clusters =  6
+n_clusters = 4
 
 #KMeans clustering
 kmeans = KMeans(n_clusters=n_clusters, init='k-means++', random_state=42, n_init=10)
 kmeans.fit(X)
 cluster_centers = kmeans.cluster_centers_
+num_features = cluster_centers.shape[1]
+print("Number of features in each center point:", num_features)
 
 #Euclidean distances from cluster centers
 distances = pairwise_distances(cluster_centers, cluster_centers)
