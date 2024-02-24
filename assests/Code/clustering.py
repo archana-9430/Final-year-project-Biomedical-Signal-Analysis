96bda973e214e310a9b62a92333d94b31c7e28f6
Archana Kumari <78868726+archana-9430@users.noreply.github.com>
2024-02-24 20:54:48 +0530

    add features and a method for clustering

---
+++ assests/Code/clustering.py
@@ -0,0 +1,45 @@
+'''cluster the segments of patients based on their annotations and
+then find the Euclidean distances from the cluster centers. '''
+
+from imported_files.paths_n_vars import intra_annotated_file
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.metrics import pairwise_distances
+
+df = pd.read_csv(intra_annotated_file)
+annotations = df.iloc[1].values
+
+# Drop patient IDs and annotations
+data = df.drop([0, 1])
+X = data.values.astype(float)
+
+#Elbow method
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i, init='k-means++', random_state=42, n_init=10)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+
+plt.plot(range(1, 11), wcss)
+plt.title('Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+#Optimal number of clusters from the elbow method plot
+n_clusters =  6
+
+#KMeans clustering
+kmeans = KMeans(n_clusters=n_clusters, init='k-means++', random_state=42, n_init=10)
+kmeans.fit(X)
+cluster_centers = kmeans.cluster_centers_
+
+#Euclidean distances from cluster centers
+distances = pairwise_distances(X, cluster_centers)
+print("Euclidean Distances from Cluster Centers: ")
+print(distances)
+features_df = pd.DataFrame(distances)
+features_df = features_df.T
+features_df.to_csv("cluster_euclidean_distances.csv", index = False)
--- assests/Code/imported_files/statistical_feature.py
+++ assests/Code/imported_files/statistical_feature.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
-import EntropyHub as EH
+import nolds
 
 from scipy.stats import entropy, skew, kurtosis
 import scipy.signal as signal
+
 
 def shannon_entropy(segment):
     # Calculate the probability distribution
@@ -14,6 +15,31 @@ def shannon_entropy(segment):
     # Compute the Shannon entropy
     H = entropy(p_normalized, base=2)
     return H
+
+def symbolize(data, num_levels):
+    min_val = np.min(data)
+    max_val = np.max(data)
+    step_size = (max_val - min_val) / num_levels
+    symbols = np.floor((data - min_val) / step_size).astype(int)
+    return symbols
+
+def permutation_entropy(data, order, num_levels):
+    symbols = symbolize(data, num_levels)
+    patterns = [tuple(symbols[i:i+order]) for i in range(len(symbols) - order + 1)]
+    _, counts = np.unique(patterns, return_counts=True, axis=0)
+    probabilities = counts / len(patterns)
+    entropy = -np.sum(probabilities * np.log2(probabilities))
+    return entropy
+
+def svd_entropy(data):
+    # Convert pandas Series to numpy array
+    # data_array = data.values
+    data_array = data.values.reshape(-1, 1)
+    _, s, _ = np.linalg.svd(data_array)
+    norm_s = s / np.sum(s)
+    entropy = -np.sum(norm_s * np.log(norm_s))
+    
+    return entropy
 
 def mean_psd(segment, fs):
     f, psd = signal.welch(segment, fs=fs)
@@ -30,9 +56,6 @@ def mean_psd(segment, fs):
     spectral_entropy = -np.sum(normalized_psd * np.log2(normalized_psd))
     return std_psd, dominant_frequency, spectral_entropy
 
-def sample_entropy(segment):
-    Samp, _ = EH.SampEn(segment, m = 4)
-    return Samp
     
 def rms(segment):
     rms = np.sqrt(np.mean(segment**2))
@@ -46,16 +69,21 @@ def interquartile_range(segment):
 def statistical(segment):
     features = {}
     #Time domain:
-    features['mean'] = np.mean(segment)
+    # print(type(segment))
     features['population_std'] = np.std(segment)
     features['sample_std'] = np.std(segment,  ddof=1)
-    features['rms'] = rms(segment)
     features['skewness'] = skew(segment)
     features['kurtosis'] = kurtosis(segment)
+    features['permutation_entropy'] = permutation_entropy(segment, 3, 10)
+    features['svd_entropy'] = svd_entropy(segment)
+    features['mean'] = np.mean(segment)
+  
+    features['rms'] = rms(segment)
+    
     features['interquartile_range'] = interquartile_range(segment)
     
     features['Shannon entropy'] = shannon_entropy(segment)
-    # features['sample_entropy'] = sample_entropy(segment)
+
 
     #Frequency domain
     features['std_psd'], features['dominant_freq'], features['spectral_entropy'] = mean_psd(segment, 125)
