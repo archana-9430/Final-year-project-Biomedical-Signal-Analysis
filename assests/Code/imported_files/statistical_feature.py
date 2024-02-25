ac2e357ff6084c81adb5b1befa84a3740b6f6d00
Archana Kumari <78868726+archana-9430@users.noreply.github.com>
2024-02-25 15:35:42 +0530

    Increase time+freq domain features to 17

--- assests/Code/imported_files/statistical_feature.py
+++ assests/Code/imported_files/statistical_feature.py
@@ -4,8 +4,10 @@ import nolds
 
 from scipy.stats import entropy, skew, kurtosis
 import scipy.signal as signal
+from scipy.signal import find_peaks
+from scipy.fft import fft
 
-
+#TIME DOMAIN
 def shannon_entropy(segment):
     # Calculate the probability distribution
     p = np.histogram(segment)[0]
@@ -41,22 +43,25 @@ def svd_entropy(data):
     
     return entropy
 
-def mean_psd(segment, fs):
-    f, psd = signal.welch(segment, fs=fs)
-    '''Welch's method: Divide the signal into overlapping segments,
-    computing the Fourier transform of each segment,
-    and averaging the squared magnitudes of the resulting spectra to obtain the PSD estimate.
-    '''
-    # mean_psd = np.mean(psd)
-    std_psd = np.std(psd)
-    #accessing the maximum frequency from the index of the maximum power value in the PSD array
-    dominant_frequency = f[np.argmax(psd)]
-     # Calculate spectral entropy
-    normalized_psd = psd / np.sum(psd)  # Normalize PSD
-    spectral_entropy = -np.sum(normalized_psd * np.log2(normalized_psd))
-    return std_psd, dominant_frequency, spectral_entropy
-
+def first_derivative_std(signal):
+    first_derivative = np.gradient(signal)
+    std_derivative = np.std(first_derivative)
+    return std_derivative    
     
+def zero_crossing_rate(signal):
+    zero_crossings = 0
+    for i in range(1, len(signal)):
+        # Check if the signal changes sign
+        if (signal[i] >= 0 and signal[i - 1] < 0) or (signal[i] < 0 and signal[i - 1] >= 0):
+            zero_crossings += 1
+    # Return the zero crossing rate
+    return zero_crossings / (len(signal) - 1)    
+
+def mean_absolute_power(signal):
+    abs_signal = np.abs(signal)
+    map_signal = np.mean(abs_signal ** 2)
+    return map_signal
+
 def rms(segment):
     rms = np.sqrt(np.mean(segment**2))
     return rms
@@ -64,7 +69,42 @@ def rms(segment):
 def interquartile_range(segment):
     q3, q1 = np.percentile(segment, [75 ,25])
     return (q3 - q1)
+ 
+ 
+def extract_rr_intervals(ppg_segment, sampling_rate):
+    peaks, _ = find_peaks(ppg_segment, distance=sampling_rate/2)
+    # Compute the time differences between consecutive peaks to obtain RR intervals
+    rr_intervals = np.diff(peaks) / sampling_rate
+    return rr_intervals   
+# RMSSD (Root Mean Square of Successive Differences) is a measure of heart rate variability
+def rmssd(segment):
+    rr_intervals = extract_rr_intervals(segment, 125)
+    successive_diff = np.diff(rr_intervals)
+    squared_diff = successive_diff ** 2
+    mean_squared_diff = np.mean(squared_diff)
+    rmssd_value = np.sqrt(mean_squared_diff)
+    return rmssd_value
     
+#FREQUENCY DOMAIN
+def mean_psd(segment, fs):
+    f, psd = signal.welch(segment, fs=fs)
+    '''Welch's method: Divide the signal into overlapping segments,
+    computing the Fourier transform of each segment,
+    and averaging the squared magnitudes of the resulting spectra to obtain the PSD estimate.
+    '''
+    std_psd = np.std(psd)
+    # mean_psd = np.mean(psd)
+    #accessing the maximum frequency from the index of the maximum power value in the PSD array
+    dominant_frequency = f[np.argmax(psd)]
+    normalized_psd = psd / np.sum(psd)
+    spectral_entropy = -np.sum(normalized_psd * np.log2(normalized_psd))
+    return std_psd, dominant_frequency, spectral_entropy
+
+def fourier_kurtosis(signal):
+    fourier_transform = fft(signal)
+    magnitude_spectrum = np.abs(fourier_transform)
+    kurt = kurtosis(magnitude_spectrum)
+    return kurt
 
 def statistical(segment):
     features = {}
@@ -75,17 +115,17 @@ def statistical(segment):
     features['skewness'] = skew(segment)
     features['kurtosis'] = kurtosis(segment)
     features['permutation_entropy'] = permutation_entropy(segment, 3, 10)
-    features['svd_entropy'] = svd_entropy(segment)
-    features['mean'] = np.mean(segment)
-  
-    features['rms'] = rms(segment)
-    
-    features['interquartile_range'] = interquartile_range(segment)
-    
+    # features['svd_entropy'] = svd_entropy(segment)
     features['Shannon entropy'] = shannon_entropy(segment)
-
+    features['first_derivative_std'] = first_derivative_std(segment)
+    features['zero_crossing_rate'] = zero_crossing_rate(segment)
+    features['interquartile_range'] = interquartile_range(segment)
+    features['mean_absolute_power'] = mean_absolute_power(segment)
+    features['mean'] = np.mean(segment)
+    features['rms'] = rms(segment)
+    features['rmssd'] = rmssd(segment)
 
     #Frequency domain
     features['std_psd'], features['dominant_freq'], features['spectral_entropy'] = mean_psd(segment, 125)
-    
+    features['fourier_kurtosis'] = fourier_kurtosis(segment)
     return features
