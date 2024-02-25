4d5879366e9b304408eb29de352e22ccd1d1f804
Shwashwat <shwashwatdas@gmail.com>
2024-02-25 21:29:26 +0530

    updated rf

--- assests/Code/8.RF_model.py
+++ assests/Code/8.RF_model.py
@@ -1,4 +1,4 @@
-from imported_files.paths_n_vars import features_file, intra_annotated_file
+from imported_files.paths_n_vars import features_file, intra_annotated_file, all_features_file
 
 rand_state = 54
 test_fraction = 0.5
@@ -46,18 +46,17 @@ def k_fold_s_crossval(x_train_data , y_train_data , k_value , classifier):
     print("Avg accuracy on train set: {}".format(result.mean()))
 
 
-def test_n_results(x_test_data , y_test_data , classifier):
+def test_n_results(x_test_data , y_test_data , classifier , description:str = ""):
 
     # Now TESTING the model and showing the results:
     # confusion matrix 
     test_pred_decision_tree = classifier.predict(x_test_data)
     confusion_matrix = metrics.confusion_matrix(y_test_data , test_pred_decision_tree)
     class_list = np.unique(y_test_data)
-    matrix_df = pd.DataFrame(confusion_matrix)
     ax = plt.axes()
-    sns.set(font_scale=1.3)
-    sns.heatmap(matrix_df , annot = True , fmt = "g" , ax = ax , cmap = "magma")
-    ax.set_title('Confusion Matrix - Random Forest')
+    sns.set_theme(font_scale=1.3)
+    sns.heatmap(confusion_matrix , annot = True , fmt = "g" , ax = ax , cmap = "magma")
+    ax.set_title('Confusion Matrix - Random Forest: ' + description)
     ax.set_xlabel("Predicted label" , fontsize = 15)
     ax.set_xticklabels(class_list)
     ax.set_ylabel("True Label", fontsize = 15)
@@ -68,21 +67,30 @@ def test_n_results(x_test_data , y_test_data , classifier):
     print("\nClassification Report:\n")
     print(classification_report(y_test_data, test_pred_decision_tree))
     print("\nAvg score on test dataset = {}".format(classifier.score(x_test_data , y_test_data)))
-    print("\nNumber of Estimators = {}".format(num_trees))
-    print("Split Criteria = {}".format(split_criteria))
     print(classifier)
 
-def rf_model(annotated_file , _features_file):
+def rf_model( local_features_file, annotated_file : str = ""  , description : str = ""):
     # get the dataset from the files
-    features = pd.read_csv(_features_file)
-    annotated_data = pd.read_csv(annotated_file)
-    labels = annotated_data.iloc[0] # this will exract the annotation 2nd row
+    features = pd.DataFrame()
+    labels = pd.DataFrame()
+    if annotated_file != "":
+        features = pd.read_csv(local_features_file)
+        labels = pd.read_csv(annotated_file).iloc[0] # this will exract the annotation 2nd row
+
+    else:
+        features = pd.read_csv(local_features_file)
+        labels = features['annotation']
+        features.drop(['annotation'] , axis = 1)
     
     # split the dataset using test_train_split() function
     x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size = test_fraction, random_state = rand_state, stratify = labels)
 
+    print(f"y_train :: \n{y_train}")
+    print(f"y_test :: \n{y_test}")
     clf = create_train_classifier(x_train , y_train)
     k_fold_s_crossval(x_train , y_train , k , clf)
-    test_n_results(x_test , y_test , clf)
+    test_n_results(x_test , y_test , clf , description)
 
-rf_model(intra_annotated_file , features_file)
+rf_model(features_file , intra_annotated_file , description = "w/o AE features")
+rf_model( all_features_file , description = "with all features")
+
