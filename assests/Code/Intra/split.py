f3377597c17e0d4bcd696372d99aa5dae0087236
Shwashwat <shwashwatdas@gmail.com>
2024-03-08 13:49:42 +0530

    update code

--- assests/Code/Intra/split.py
+++ assests/Code/Intra/split.py
@@ -4,18 +4,13 @@ Script shuffles then splits the annotated data to Train and Test parts in about 
 import sys
  
 sys.path.insert(0, 'F:/Shwashwat/B_Tech_ECE/project/Github folder/Final-year-project-Biomedical-Signal-Analysis/assests/Code/imported_files')
-import os
-import shutil
-from paths_n_vars import annotated_folder
+from paths_n_vars import annotation_folder
 from merge import merge_csv
 
-src_folder = "../" + annotated_folder
+src_folder = "../" + annotation_folder
         
 def merge():
     merge_csv(src_folder, "combined_annotated.csv" , save = True)
 
 merged_df_intra = merge()
 
-from icecream import ic 
-  
-ic.configureOutput(includeContext=True)
--- assests/Code/imported_files/paths_n_vars.py
+++ assests/Code/imported_files/paths_n_vars.py
@@ -13,6 +13,7 @@ re_annotation_folder = '5.New_annotated_data'
 features_folder = '6.Features_extracted'
 features_file = '6.Features_extracted/features.csv'
 ae_features_file = '6.Features_extracted/AE_features.csv'
+ae_derivative_features_file = '6.Features_extracted/AE_derivative_features.csv'
 all_features_file = '6.Features_extracted/all_features.csv'
 min_max_features_file = '6.Features_extracted/all_features_minmax.csv'
 zscore_features_file = '6.Features_extracted/all_features_zscore.csv'
@@ -25,6 +26,8 @@ inter_train_file = 'Inter/Train/combined_annotated_train.csv'
 intra_annotated_file = 'Intra/combined_annotated.csv'
 encoder_save = 'AE_model/encoder_model.keras'
 decoder_save = 'AE_model/decoder_model.keras'
+encoder_derivative_save = 'AE_model/encoder_derivative_model.keras'
+decoder_derivative_save = 'AE_model/decoder_derivative_model.keras'
 
 # variables
 sampling_frequency = 125 # in Hertz
