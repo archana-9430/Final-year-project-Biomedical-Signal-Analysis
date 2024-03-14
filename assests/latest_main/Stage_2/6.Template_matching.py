768e8c1fcfcd0c9fe693916c9ee776fb3c1f5418
Shwashwat <shwashwatdas@gmail.com>
2024-03-14 10:55:07 +0530

    Update template matching

--- assests/latest_main/Stage_2/6.Template_matching.py
+++ assests/latest_main/Stage_2/6.Template_matching.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.signal import correlate
+from scipy.signal import correlate , correlation_lags
 
 # Load the template and PPG data from CSV files
 template_df = pd.read_csv('Templates/template_0.csv')
@@ -10,7 +10,6 @@ ppg_df = pd.read_csv('partly_corrupted.csv',skiprows=1)
 # Assuming the template and PPG data are in 'value' column of the DataFrame, adjust as per your actual data structure
 template_data = template_df.values
 ppg_data = ppg_df.values
-
 # Visualize the template and PPG data
 plt.figure(figsize=(12, 6))
 plt.subplot(2, 1, 1)
@@ -31,26 +30,40 @@ plt.show()
 # Calculate the cross-correlation between the template and a portion of the PPG data
 # You may adjust the portion of data used here for your analysis
 window_size = len(template_data)
-shift = int(0.2*125)
+shift = int(0.2*1250)
 num_windows = int(ppg_df.shape[0]/window_size)
-print(num_windows)
+print('number of windows = ',num_windows)
+
 for _,col in ppg_df.items():
-    for i in range(num_windows): 
-        correlation = correlate(col.values[i*shift:i*shift+window_size].reshape(-1,1), template_data, mode='full')
+    for i in range(num_windows):
+        window = col.values[i*shift:i*shift+window_size]
+        print('shape of window = ', window.shape)
+        print('shape of template_data = ', template_data.shape)
+        correlation_values = correlate(window.reshape(-1,1), template_data.reshape(-1,1), mode='same')
+        lags = correlation_lags(len(window), len(template_data), mode='same')
+        
 
         # Find the peak in the correlation to determine the appropriate window size
-        max_index = np.argmax(correlation)
-        peak_value = correlation[max_index]
+        max_index = np.argmax(correlation_values)
+        peak_value = correlation_values[max_index]
 
         print("Peak value of correlation:", peak_value)
         print("Index of peak:", max_index)
 
         # Visualize the correlation result
-        plt.figure(figsize=(8, 4))
-        plt.plot(correlation)
-        plt.title('Cross-correlation Result')
-        plt.xlabel('Index')
-        plt.ylabel('Correlation Value')
-        plt.axvline(x=max_index, color='r', linestyle='--', label='Peak')
+        fig, (ax_orig, ax_template, ax_corr) = plt.subplots(3, 1, figsize=(12 , 6))
+        ax_orig.plot(window)
+        ax_orig.set_title('Original signal')
+        ax_orig.set_xlabel('Sample Number')
+        ax_template.plot(template_data)
+        ax_template.set_title('Template')
+        ax_template.set_xlabel('Sample Number')
+        ax_corr.plot(lags, correlation_values, label = 'scipy function')
+        ax_corr.set_title('Cross-correlated signal')
+        ax_corr.set_xlabel('Lag')
+        ax_orig.margins(0, 0.1)
+        ax_template.margins(0, 0.1)
+        ax_corr.margins(0, 0.1)
+        fig.tight_layout()
         plt.legend()
         plt.show()
