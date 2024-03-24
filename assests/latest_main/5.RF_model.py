9661032cea69d8140e41c4150e87399ffd332d5b
Shwashwat <shwashwatdas@gmail.com>
2024-03-24 15:05:45 +0530

    add data

--- assests/latest_main/5.RF_model.py
+++ assests/latest_main/5.RF_model.py
@@ -1,5 +1,5 @@
 from imported_files.paths_n_vars import stats_features, filtered_merged, feature_merged
-from imported_files.ml_helper import Ml_Model
+from imported_files.ml_helper import Ml_helper
 
 rand_state = 54
 test_fraction = 0.5
@@ -21,22 +21,20 @@ from sklearn.model_selection import train_test_split
 from collections import Counter
 # ~~~~~~~~~~~~END LIBRARIES~~~~~~~~~~~~~~~~~~~~~
 
-def rf_model_function( local_features_file, annotated_file , description : str = ""):
+def rf_model_function( local_features_file , description : str = ""):
     # get the dataset from the files
-    features = pd.read_csv(local_features_file)
-    labels = pd.read_csv(annotated_file).iloc[0] # this will exract the annotation 2nd row    
+    features_df = pd.read_csv(local_features_file)
+    labels = features_df['annotation'] # this will exract the annotation 2nd row    
     class_list = np.unique(labels)
-
-    if local_features_file == annotated_file:
-        features.drop(index=0, inplace=True)
-        features = features.T
         
-    if 'annotation' in features.columns :
-        features.drop(['annotation'] , axis = 'columns' , inplace = True)
+    if 'annotation' in features_df.columns :
+        features_df.drop(['annotation'] , axis = 'columns' , inplace = True)
 
-    features_train, features_test, y_train, y_test = train_test_split(features, labels, test_size = test_fraction, random_state = rand_state, stratify = labels)
+    assert not 'annotation' in features_df.columns
 
-    if 'PatientID' in features.columns:
+    features_train, features_test, y_train, y_test = train_test_split(features_df, labels, test_size = test_fraction, random_state = rand_state, stratify = labels)
+
+    if 'PatientID' in features_df.columns:
         pID_train = features_train['PatientID']
         pID_test = features_test['PatientID']
         x_train = features_train.drop(['PatientID'] , axis = 'columns')
@@ -52,18 +50,21 @@ def rf_model_function( local_features_file, annotated_file , description : str =
     print(f"Train instances : {num_instances_train}")
     print(f"Test instances : {num_instances_test}")
 
+    assert not ('annotation' in x_train.columns or 'PatientID' in x_train.columns)
     # create and train classifier
-    rf_model = Ml_Model('rf' , n_estimators = num_trees , random_state = rand_state \
+    rf_model = Ml_helper('rf' , n_estimators = num_trees , random_state = rand_state \
                                   , criterion = split_criteria, verbose = 1)
     rf_model.classifier.fit(x_train , y_train)
     rf_model.k_fold_strat_crossval(x_train , y_train , k , rand_state)
     y_pred = rf_model.test_n_results(x_test , y_test , description)
     
-    if 'PatientID' in features.columns:
+    allMissClassifications = []
+    missClasslist = []
+    if 'PatientID' in features_df.columns:
         # identifying miss-classisfied segments and saving them
         classification_rep = tuple(zip(pID_test.values , y_test.values , y_pred))
-        num_patients = np.unique( [x.split('_')[0] for x in pID_test.values])
-        print('number of patients = ',num_patients)
+        unique_patients = np.unique( [x.split('_')[0] for x in pID_test.values])
+        print('number of patients = ',len(unique_patients))
         dataset = pd.read_csv('annotated_merged.csv')
 
         dataset[[x[0] for x in classification_rep if x[2] == 0] ].to_csv(path_or_buf = 'Stage_2/clean.csv',index = False)
@@ -73,23 +74,37 @@ def rf_model_function( local_features_file, annotated_file , description : str =
 
         allMissClassifications = [x for x in classification_rep if x[1] != x[2]] # x[1] -> true labels :: x[2] -> predicted labels
         
-        
-        missClasslist = []
         for i in range(len(class_list)):
             innerList = []
             for j in range(len(class_list)):
                 temp = [x[0] for x in allMissClassifications if ((x[1] == i) and (x[2] == j))] 
                 if len(temp) > 0:
                     innerList.append(temp)
-                    dataset[temp].to_csv(path_or_buf = f'4.missclassifications/missclassification_Of{i}_to_{j}.csv',index = False)
-            if len(innerList) > 0:
-                missClasslist.append(innerList)
+                    dataset[temp].to_csv(path_or_buf = f'4.missclassifications/missclassification_Of_{i}_to_{j}.csv',index = False)
+            missClasslist.append(innerList)
                 
     else:
         print('Patient ID not given so cannot identify missclassified patients...')
+        missClasslist = None
+
+    return np.array(allMissClassifications) , missClasslist
     
 print("\n~~~~~ RF:: W/O AE FEATURES ~~~~~")
-rf_model_function(stats_features, filtered_merged, description = "Statistical features")
+list_stats , stat_miss_class = rf_model_function(stats_features, description = "Statistical features")
 
 print("\n~~~~~ RF:: WITH ALL FEATURES ~~~~~")
-rf_model_function(feature_merged, filtered_merged, description = "All features")
+list_all , all_miss_class = rf_model_function(feature_merged, description = "All features")
+# for i in range(len(list_all)):
+#     if len(list_all[i]):
+#         for x in list_all[i]:
+#             if x not in list_stats[i]:
+#                 print(x)
+only_ae = []
+for x in list_all:
+    if x[0] not in list_stats[:,0]:
+        only_ae.append(x.tolist())
+    
+# print(f'{only_ae = }')
+print(f'{list_stats.shape = }')
+print(f'{list_all.shape = }')
+print(f'{len(only_ae) = }')
